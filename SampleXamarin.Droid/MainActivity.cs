d83f2b9447deec4e43a2d0ff9d862cac5f394cef
MiyaharaFumina <fumina@FuminaMac.local>
2016-12-18 18:29:47 +0900

    using Prism Template.

---
+++ SampleXamarin.Droid/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Prism.Unity;
+
+namespace SampleXamarin.Droid
+{
+	[Activity(Label = "SampleXamarin.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+
+			LoadApplication(new App(new AndroidInitializer()));
+		}
+	}
+
+	public class AndroidInitializer : IPlatformInitializer
+	{
+		public void RegisterTypes(IUnityContainer container)
+		{
+
+		}
+	}
+}
